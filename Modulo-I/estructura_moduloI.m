775cf3bd847b3d1cf29bbc0e80983dfd56d0af6b
Geomar Perales <per3app@gmail.com>
2018-10-21 10:30:35 -0500

    Rename estructura_moduloI.m to Modulo-I/estructura_moduloI.m

---
+++ Modulo-I/estructura_moduloI.m
@@ -0,0 +1,8 @@
+clear all
+clc
+
+%Modulo I : Estructura
+%Scripts
+%Generación de variables
+%Manipulación de variables
+%Gráfico básico
--- estructura_moduloI.m
+++
@@ -1,8 +0,0 @@
-clear all
-clc
-
-%Modulo I : Estructura
-%Scripts
-%Generación de variables
-%Manipulación de variables
-%Gráfico básico
