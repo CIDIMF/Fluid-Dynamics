5d42e7f54b17293c2f53a689695bcc5c7a12ce5a
Geomar Perales <per3app@gmail.com>
2018-10-21 09:46:46 -0500

    Add files via upload
    añado el achivo estructura del curso

---
+++ estructura_curso.m
@@ -0,0 +1,10 @@
+clear all
+clc
+
+%ESTRUCTURA DEL CURSO
+
+%Modulo 1. Variables, scripts and operations
+%Modulo 2. Visualization and programming
+%Modulo 3. Solving equations and curve fifting
+%Modulo 4. Advanced methods
+%Modulo 5. file I/O
