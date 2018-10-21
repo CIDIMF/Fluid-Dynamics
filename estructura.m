281797da2c745426ecc4bd5e7f71806162f72940
Geomar Perales <per3app@gmail.com>
2018-10-21 09:26:39 -0500

    Add files via upload
    añado estructura del proyecto

---
+++ estructura.m
@@ -0,0 +1,24 @@
+clear all
+clc
+
+%Contenido:
+
+%1. Conceptos fundamentales y propiedades de los fluidos
+%1.1. Propiedades de los fluidos
+% Densidad, peso especifico, volumen especifico, densidad relativa
+% peso especifico relativo, temperatura, presion, viscosidad
+%viscosidad cinematica, gas perfecto o gas ideal, 
+
+%2. Hidrostatica
+%2.1. variacion de presion en un fluido incomprensible en reposo
+%2.2. variacion de presion en un fluido comprensible en reposo
+%2.3. presion atmosferica local
+%2.4. transductor de presión
+
+%3. Ecuaciones fundamentales de la mecanica de fluidos
+%3.1. aspectos basicos para el analisis de flujo
+%3.2. analisis integral del movimiento de los fluidos
+%3.3. analisis integral de la ec. de conservacion de masa (continuidad)
+
+
+%4. analisis dimensional y similitud
