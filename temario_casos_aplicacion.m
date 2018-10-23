3141dd818e480d95e55a003c76d0bdffc212bd7c
Geomar Perales <per3app@gmail.com>
2018-10-23 13:08:24 -0500

    Add files via upload

--- temario_casos_aplicacion.m
+++ temario_casos_aplicacion.m
@@ -6,22 +6,20 @@ clc
 %1. Conceptos fundamentales y propiedades de los fluidos
 %1.1. Propiedades de los fluidos
 % Densidad, peso especifico, volumen especifico, densidad relativa
-% peso especifico relativo, temperatura, presion, viscosidad
-%viscosidad cinematica, gas perfecto o gas ideal, 
+% temperatura, presion, viscosidad, esfuerzo cortante de un fluido
+%ley de sutherland, viscosidad cinematica
 
 %2. Hidrostatica
 %2.1. variacion de presion en un fluido incomprensible en reposo
 %2.2. variacion de presion en un fluido comprensible en reposo
-%2.3. presion atmosferica local
-%2.4. transductor de presión
+%2.3. transductor de presión : Galgas extensométricas
 
 %3. Ecuaciones fundamentales de la mecanica de fluidos
 %3.1. aspectos basicos para el analisis de flujo
-%3.2. analisis integral del movimiento de los fluidos
-%3.3. analisis integral de la ec. de conservacion de masa (continuidad)
-%3.4. Ec. de transporte de reynolds
-%3.5. analisis integral de la ec. de cantidad de movimiento
-%3.6. analisis integral de la ec. de energia
+%3.2. analisis integral de la ec. de conservacion de masa (continuidad)
+%3.3. Ec. de transporte de reynolds
+%3.4. analisis integral de la ec. de cantidad de movimiento
+%3.5. analisis integral de la ec. de energia
 
 %4. analisis dimensional y similitud
 %4.1. teorima PI de buckingham
